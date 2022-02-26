628c46585a8faaf020ee997ae68979862c78201f
Gabriel Fonseca <gdfonseca@leroymerlin.com.br>
2022-02-26 12:46:46 -0300

    feat(mergeSort): add merge sort feature

---
+++ src/MergeSorter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Fnsc\Katas;
+
+class MergeSorter
+{
+    private const MINIMUM_NUMBER_OF_ELEMENTS = 2;
+
+    public function sort(array $data): array
+    {
+        $elementsQuantity = count($data);
+
+        if (self::MINIMUM_NUMBER_OF_ELEMENTS > $elementsQuantity) {
+            return $data;
+        }
+
+        $middleIndex = floor($elementsQuantity / 2);
+        $leftArray = array_slice($data, 0, $middleIndex);
+        $rightArray = array_slice($data, $middleIndex, $elementsQuantity);
+
+        return $this->mergeSort($this->sort($leftArray), $this->sort($rightArray));
+    }
+
+    private function mergeSort(array $leftArray, array $rightArray): array
+    {
+        $result = [];
+        $leftIndex = 0;
+        $rightIndex = 0;
+
+        while ($leftIndex < count($leftArray) && $rightIndex < count($rightArray)) {
+            if ($leftArray[$leftIndex] < $rightArray[$rightIndex]) {
+                $result[] = $leftArray[$leftIndex];
+                $leftIndex += 1;
+
+                continue;
+            }
+            $result[] = $rightArray[$rightIndex];
+            $rightIndex += 1;
+        }
+
+        return array_merge(
+            $result,
+            array_slice($leftArray, $leftIndex),
+            array_slice($rightArray, $rightIndex)
+        );
+    }
+}
---
+++ tests/Unit/MergeSorterTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Fnsc\Katas\MergeSorter;
+use PHPUnit\Framework\TestCase;
+
+class MergeSorterTest extends TestCase
+{
+    public function testShouldOrderGivenArray(): void
+    {
+        // Set
+        $expected = $data = [12,3,16,6,5,1];
+        sort($expected);
+        $sorter = new MergeSorter();
+
+        // Actions
+        $result = $sorter->sort($data);
+
+        // Assertions
+        $this->assertSame($expected, $result);
+    }
+}
