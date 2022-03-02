4037cb086a2e999a31b0956e1b01beffc5a2135e
Gabriel Fonseca <gdfonseca@leroymerlin.com.br>
2022-03-02 20:53:20 -0300

    katas(dynamicProgramming): fibonacci with memoize

---
+++ tests/Unit/FibonacciTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Fnsc\Katas\Fibonacci;
+use PHPUnit\Framework\TestCase;
+
+class FibonacciTest extends TestCase
+{
+    public function testShouldCalculateFibonacciNumber(): void
+    {
+        // Set
+        $fibonacci = new Fibonacci();
+        $memo = [];
+
+        // Actions
+        $result = $fibonacci->calculate(50, $memo);
+
+        // Assertions
+        $this->assertSame(12586269025, $result);
+    }
+}
